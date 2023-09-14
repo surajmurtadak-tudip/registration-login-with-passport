02463fe235bb7063d6f941cd29543481ff15183f
surajmurtadak-tudip <suraj.murtadak@tudip.com>
2023-09-14 21:07:46 +0530

    registration & login logic added

--- index.js
+++ index.js
@@ -1,33 +1,69 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const { error } = require("console");
 const app = express();
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 mongoose.connect("mongodb://localhost:27017/passportData");
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static("public"));
+app.use(session({
+    secret : "ACBD1234",
+    resave : false,
+    saveUninitialized : false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const userSchema = new mongoose.Schema({
     username : String,
     password : String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const UserPassport = new mongoose.model("UserPassport",userSchema);
 
-app.get("/register",(req,res)=>{
-    const user1 = new UserPassport({
-                username : "Suraj",
-                password : "12345678"
+passport.use(UserPassport.createStrategy());
+passport.serializeUser(UserPassport.serializeUser());
+passport.deserializeUser(UserPassport.deserializeUser());
+
+
+app.post("/registration",(req,res)=>{
+    let userName = req.body.username;
+    let userPass = req.body.password;
+    UserPassport.register({username:userName},userPass,(err,user)=>{
+        if(err){
+            res.send(`error : ${err}`);
+        }
+        else{
+            passport.authenticate("local")(req,res,()=>{
+                res.send(`successfully register`);
             });
-            user1.save()
-            .then((data)=>{
-                res.send(`data save successfully`);
-            })
-            .catch((err)=>{
-                res.send(`something went wrong: ${err}`);
-            });
+        }
+    });
 });
 
-app.get("/login",(req,res)=>{
-    res.send("Login page");
+app.post("/login",(req,res)=>{
+    const user1 = new UserPassport({
+        username : req.body.username,
+        password : req.body.password
+    });
+    req.login(user1,(err)=>{
+        if(err){
+            res.send(`error : ${err}`);
+        }
+        else{
+            passport.authenticate("local")(req,res,()=>{
+                res.send("login successfull");
+            });
+        }
+    });
 });
 
 app.listen(4000,()=>console.log("server up and running"));
